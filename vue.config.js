3f420a1cbb8eee33d751e3b8d52d7aa5281cae7f
michael menebraye <michael@dufuna.org>
2023-01-09 20:04:44 +0100

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/23it/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/23it/" : "/",
   transpileDependencies: true
 })
