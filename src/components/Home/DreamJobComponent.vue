bf2b6c038f94ff9d23d93ed06cabc1bf0c6d8efd
michael menebraye <michael@dufuna.org>
2023-01-16 09:17:28 +0100

    added firesrore data to filter/search

--- src/components/Home/DreamJobComponent.vue
+++ src/components/Home/DreamJobComponent.vue
@@ -21,56 +21,40 @@
             </div>
         </div>
 
-
-
-        <div class="row g-0  my-4" v-for="(jobs, index) in filteredItems" style="background: #4C417E;">
+        <router-link :to="'/'" class="row g-0  my-4 rounded text-decoration-none" v-for="(jobs, index) in filteredItems" style="background: #4C417E;">
             <div class="col-md-2 p-5 text-md-start text-sm-center">
                 <img src="../../assets/img/mc-donalds.png" class="img-fluid rounded-start" alt="...">
             </div>
+
             <div class="col-md-10 py-5">
                 <div class="card-body">
                     <div class="card-text row text-white">
-                        <p class="col card-title">{{ jobs['company-name'] }}</p>
+                        <p class="col card-title">{{ jobs.company_name }}</p>
                         <p class="col card-title text-end me-3">2d ago</p>
                     </div>
-                    <p class="card-text text-white mt-3">{{ jobs['job-title'] }}</p>
+                    <p class="card-text text-white mt-3">{{ jobs.job_title }}</p>
                     <p class="card-text d-flex gap-3 mt-3 text-white">
-                        <small class="">{{ jobs['remote-location'] }}</small> |
-                        <small class="">{{ jobs['country-location'] }}</small>
-                        <small class="">{{ jobs['job-time'] }}</small>
-                        <small class="">{{ jobs['job-price'] }}</small>
+                        <small class="">{{ jobs.remotive }}</small> |
+                        <small class="">{{ jobs.location }}</small>
+                        <small class="">{{ jobs.job_type }}</small>
+                        <small class="">{{ jobs.salary }}</small>
                     </p>
                 </div>
             </div>
-        </div>
+        </router-link>
+
         <div class="button text-dark float-end">
             Find a Job
             <img src="../../assets/arrow/dark-arrow-right.png" style="height: 20px;" class="img-fluid" alt="">
         </div>
-
-        <!-- <div>
-            <input type="text" v-model="searchText" placeholder="Search..." />
-            <select v-model="searchCategory">
-                <option value="all">All</option>
-                <option value="name">Name</option>
-                <option value="age">Age</option>
-            </select>
-            <ul>
-                <li v-for="item in filteredItems" :key="item.id">{{ item['company-name'] }} - {{ item['job-title'] }}</li>
-            </ul>
-        </div> -->
-
 
     </div>
 </template>
 
 
 <script>
-
 import { collection, getDocs } from "firebase/firestore";
-import {db} from '../../firebase/config'
-
-
+import { db } from '../../firebase/config'
 
 export default {
     name: 'DreamJobComponent',
@@ -78,23 +62,12 @@ export default {
         return {
             searchText: '',
             searchCategory: 'all',
-            items: [
-                { id: 1, 'company-name': 'Mc Donald`s', 'job-title': 'Senior Product Designer', 'job-price': '$100k - $111k', 'job-time': 'Full-time', 'remote-location': 'Remote', 'country-location': 'Bulgeria only', },
-                { id: 2, 'company-name': 'Facebook', 'job-title': 'Senior backend Engineer', 'job-price': '$109k - $151k', 'job-time': 'Part-time', 'remote-location': 'Remote', 'country-location': 'England only', },
-                { id: 3, 'company-name': 'Amazon', 'job-title': 'Senior Product Designer', 'job-price': '$101k - $111k', 'job-time': 'Full-time', 'remote-location': 'Remote', 'country-location': 'Ukrain only', },
-                { id: 4, 'company-name': 'Netflix', 'job-title': 'Feontend Engineer', 'job-price': '$55k - $100k', 'job-time': 'Part-time', 'remote-location': 'Onsite', 'country-location': 'Canada only', },
-            ],
-            // jobData: [
-            //     { id: 1, 'company-name': 'Mc Donald`s', 'job-title': 'Senior Product Designer', 'job-price': '$100k - $111k', 'job-time': 'Full-time', 'remote-location': 'Remote', 'country-location': 'Bulgeria only', },
-            //     { id: 2, 'company-name': 'Facebook', 'job-title': 'Senior backend Engineer', 'job-price': '$109k - $151k', 'job-time': 'Part-time', 'remote-location': 'Remote', 'country-location': 'England only', },
-            //     { id: 3, 'company-name': 'Amazon', 'job-title': 'Senior Product Designer', 'job-price': '$101k - $111k', 'job-time': 'Full-time', 'remote-location': 'Remote', 'country-location': 'Ukrain only', },
-            //     { id: 4, 'company-name': 'Netflix', 'job-title': 'Feontend Engineer', 'job-price': '$55k - $100k', 'job-time': 'Part-time', 'remote-location': 'Onsite', 'country-location': 'Canada only', },
-            // ],
+            jobData: [],
         };
     },
     computed: {
         filteredItems() {
-            let filtered = this.items;
+            let filtered = this.jobData;
 
             if (this.searchCategory !== 'all') {
                 filtered = filtered.filter(item => item[this.searchCategory].toLowerCase().includes(this.searchText.toLowerCase()));
@@ -109,8 +82,17 @@ export default {
 
         const querySnapshot = await getDocs(collection(db, "jobs"));
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            let data = {
+                company_name: doc.data().company_name,
+                job_title: doc.data().job_title,
+                job_type: doc.data().job_type,
+                jobs_id: doc.data().jobs_id,
+                location: doc.data().location,
+                remotive: doc.data().remotive,
+                salary: doc.data().salary,
+                id: doc.id,
+            }
+            this.jobData.push(data);
         });
     }
 }
@@ -118,8 +100,6 @@ export default {
 
 
 <style scoped>
-@import "//netdna.bootstrapcdn.com/font-awesome/3.0/css/font-awesome.css";
-
 @font-face {
     font-family: KyivTypeSerif-Bold3;
     src: url(../../assets/font/KyivTypeSerif-Bold3.ttf);
